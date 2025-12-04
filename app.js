d243de3191a6893818c0de8b699f1f5a7a624cc1
Christina <christinamarieelaine@gmail.com>
2025-12-03 19:11:22 -0800

    exercise 4

--- app.js
+++ app.js
@@ -41,4 +41,19 @@ Solve Exercise 3 here:
 game.difficulty = "Medium";
 
 
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+
+const bulbasaur = pokemon.find(p => p.name === "Bulbasaur");
+
+game.party.push(bulbasaur);
+
+//console.log(game);
 
