09f6e86c00364cd21c269719e2ff13bbab5fe41d
Christina <christinamarieelaine@gmail.com>
2025-12-07 20:00:43 -0800

    exercise 21

--- app.js
+++ app.js
@@ -487,3 +487,45 @@ game.catchPokemon("pikachu"); // works
 
 console.log(game.catchPokemon("Xyzmon"));
 // "This Pokémon does not exist!"
+
+
+/*
+Exercise 21
+Dynamically construct an object with the existing `pokemon` data sorted by the different pokemon types. The object will have this structure:
+
+{
+  grass: [
+    { number: 1, name: 'Bulbasaur', type: 'grass', hp: 45, starter: true },
+    { number: 2, name: 'Ivysaur', type: 'grass', hp: 60, starter: false },
+    { number: 3, name: 'Venusaur', type: 'grass', hp: 80, starter: false },
+    * more grass type Pokemon objects...
+  ],
+  fire: [
+    { number: 4, name: 'Charmander', type: 'fire', hp: 39, starter: true },
+    * more fire type Pokemon objects...
+  ],
+  water: [
+    * water type Pokemon objects...
+  ],
+  * etc... until there is an array for every Pokemon type!
+}
+
+Log the object when it's constructed.
+
+Solve Exercise 21 here:
+*/
+
+
+const pokemonByType = {};
+
+pokemon.forEach(p => {
+  // If this type doesn't exist yet, create an empty array
+  if (!pokemonByType[p.type]) {
+    pokemonByType[p.type] = [];
+  }
+
+  // Add this pokemon to its type array
+  pokemonByType[p.type].push(p);
+});
+
+console.log(pokemonByType);
